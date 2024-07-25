6665e7886e2dbb578faa6e0d79fc1db02072b805
johnpaulrusso <jprusso93@gmail.com>
2024-07-24 20:18:53 -0500

    Improve the look of the demo page.
    Improve the look of the demo page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -47,7 +47,11 @@
 	}
 </script>
 
-<h1>Welcome to the Sverminal demo</h1>
+<div class="w-full flex flex-col justify-center items-center text-center p-4 md:p-8 gap-2 md:gap-4">
+    <h1 class="text-5xl md:text-7xl font-mono font-bold">SVERMINAL</h1>
+    <h3 class="text-sm md:text-base font-mono">Terminal emulator built on Svelte and Tailwind</h3>
+</div>
+
 <!--
 <div class="p-4">
 <Sverminal {processCommand} />
