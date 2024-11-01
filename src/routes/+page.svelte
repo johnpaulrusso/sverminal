de808541c2b69c36fc9aeb8b66f731b8b0a26423
johnpaulrusso <jprusso93@gmail.com>
2024-11-01 09:46:24 -0500

    Added a logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -232,7 +232,60 @@
 </script>
 
 <div class="w-full flex flex-col justify-center items-center text-center p-4 md:p-8 gap-2 md:gap-4">
-	<h1 class="text-5xl md:text-7xl font-mono font-bold">SVERMINAL</h1>
+	<h1 class="text-5xl md:text-7xl font-mono font-bold flex justify-around items-center gap-2">
+        <svg
+   width="48"
+   height="48"
+   viewBox="0 0 33.866666 33.866666"
+   version="1.1"
+   id="svg1"
+   xmlns="http://www.w3.org/2000/svg">
+  <defs
+     id="defs1" />
+  <g
+     id="layer1">
+    <path
+       style="fill:#000000;fill-opacity:1;stroke:none;stroke-width:1"
+       d="m 10.996713,6.5634736 6.62047,-4.600666 5.049511,3.141918 3.702974,-3.478552 7.069317,3.366341 -6.732682,-1.234325 -3.590764,4.2640296 -5.386145,-4.0396076 z"
+       id="path1" />
+    <path
+       style="fill:#000000;stroke:none;stroke-width:1;fill-opacity:1"
+       d="m 0.60191876,18.717501 6.62046964,-4.600666 5.0495116,3.141918 3.702974,-3.478552 7.069317,3.366341 -6.732682,-1.234325 -3.590764,4.26403 -5.3861456,-4.039608 z"
+       id="path1-1" />
+    <path
+       style="fill:#000000;fill-opacity:1;stroke:none;stroke-width:1"
+       d="m 10.635969,30.901593 6.62047,-4.600666 5.049511,3.141918 3.702974,-3.478552 7.069317,3.366341 -6.732682,-1.234325 -3.590764,4.26403 -5.386145,-4.039608 z"
+       id="path1-9" />
+  </g>
+</svg>
+<div>SVERMINAL</div>
+<svg
+   width="48"
+   height="48"
+   viewBox="0 0 33.866666 33.866666"
+   version="1.1"
+   id="svg1"
+   xmlns="http://www.w3.org/2000/svg">
+  <defs
+     id="defs1" />
+  <g
+     id="layer1"
+     transform="matrix(-1,0,0,1,34.040904,0)">
+    <path
+       style="fill:#000000;fill-opacity:1;stroke:none;stroke-width:1"
+       d="m 10.996713,6.5634736 6.62047,-4.600666 5.049511,3.141918 3.702974,-3.478552 7.069317,3.366341 -6.732682,-1.234325 -3.590764,4.2640296 -5.386145,-4.0396076 z"
+       id="path1" />
+    <path
+       style="fill:#000000;fill-opacity:1;stroke:none;stroke-width:1"
+       d="m 0.60191876,18.717501 6.62046964,-4.600666 5.0495116,3.141918 3.702974,-3.478552 7.069317,3.366341 -6.732682,-1.234325 -3.590764,4.26403 -5.3861456,-4.039608 z"
+       id="path1-1" />
+    <path
+       style="fill:#000000;fill-opacity:1;stroke:none;stroke-width:1"
+       d="m 10.635969,30.901593 6.62047,-4.600666 5.049511,3.141918 3.702974,-3.478552 7.069317,3.366341 -6.732682,-1.234325 -3.590764,4.26403 -5.386145,-4.039608 z"
+       id="path1-9" />
+  </g>
+</svg>
+</h1>
 	<h3 class="text-sm md:text-base font-mono">
 		Terminal emulator built on Svelte and Tailwind. Version {SVERMINAL_VERSION}.
 	</h3>
